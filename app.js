a516acbc50e41f0924f748e1602c374ea8790dac
Axel Ulmestig <axel.ulmestig@gmail.com>
2015-12-30 18:57:18 +0100

    added express server to make bluemix happy

--- app.js
+++ app.js
@@ -19,9 +19,47 @@ client.on('connect', function () {
  
 client.on('message', function (topic, message_buffer) {
 	var beacon = JSON.parse(message_buffer.toString());
+	beacon.timestamp = Math.floor(Date.now() / 1000);
 	//TODO validate beacon
 	track_beams.add_beacon(beacon, beams, update_beams);
 	setTimeout(function(){
 		track_beams.refresh_beams(beams, update_beams);
 	}, constants.BEACON_DURATION * 1000);
+});
+
+
+
+/*
+ * Bluemix refuses to run the program without a web server, so here's the template code for it...
+ *
+ */
+
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
 });
