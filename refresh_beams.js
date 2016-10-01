7b63acd5c51880d3aad16743f48562ec140398d0
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-10-01 19:33:55 +0200

    added refreshBeams function

---
+++ refresh_beams.js
@@ -0,0 +1,31 @@
+var Beam = require('./beam2.js');
+var Constants = require('./beam_constants.js');
+var reduceBeams = require('./reduce_beams.js');
+
+var refreshBeams = function(timestamp, beams) {
+        var refreshed = [];
+        beams.forEach(function(beam) {
+                var refreshedBeam = refreshBeam(timestamp, beam);
+                refreshed = refreshed.concat(refreshedBeam);
+        });
+        return refreshed;
+}
+
+var refreshBeam = function(timestamp, beam) {
+        var beamTimestamp = Beam.getTimestamp(beam);
+        var limit = timestamp - Constants.BEACON_DURATION;
+        if(beamTimestamp > limit){
+                return [beam];
+        }
+        if(beam.isPhoton) {
+                return [];
+        }
+        return refreshBeams(timestamp, [beam.beam1, beam.beam2]);
+}
+
+module.exports = function(beams) {
+        var timestamp = Math.floor(Date.now() / 1000);
+        var refreshed = refreshBeams(timestamp, beams);
+        var reduced = reduceBeams(refreshed);
+        return reduced;
+}
---
+++ test/refresh_beams_test.js
@@ -0,0 +1,49 @@
+var expect = require('chai').expect;
+var refreshBeams = require('../refresh_beams.js');
+var Beam = require('../beam2.js');
+
+describe('refresh beams', function(){
+        it('one old, one fresh', function(){
+                var old = Beam.createPhoton(0, 0);
+                old.timestamp = 0;
+                var fresh = Beam.createPhoton(100, 100);
+                var beams = [old, fresh];
+                var refreshed = refreshBeams(beams);
+
+                expect(refreshed.length).to.equal(1);
+                var remainingBeam = refreshed[0];
+                expect(remainingBeam).to.equal(fresh);
+        });
+        it('one old, one fresh in a beacon', function(){
+                var old = Beam.createPhoton(0, 0);
+                old.timestamp = 0;
+                var fresh = Beam.createPhoton(0, 0);
+                var beacon = Beam.addBeams(fresh, old);
+                var refreshed = refreshBeams([beacon]);
+
+                expect(refreshed.length).to.equal(1);
+                var remainingBeam = refreshed[0];
+                expect(remainingBeam).to.equal(fresh);
+        });
+        it('one old beacon, one fresh photon', function(){
+                /*
+                 * The beacon should get replaced with 
+                 * it's fresh photon. That photon should
+                 * then get merged with the other photon
+                 * in the list. Resulting in a list of
+                 * one beacon containing two photons.
+                 */
+                var old = Beam.createPhoton(100, 100);
+                old.timestamp = 0;
+                var fresh1 = Beam.createPhoton(0, 0);
+                var fresh2 = Beam.createPhoton(0, 0);
+                var beacon = Beam.addBeams(fresh1, old);
+                var refreshed = refreshBeams([beacon, fresh2]);
+
+                expect(refreshed.length).to.equal(1);
+                var remainingBeam = refreshed[0];
+                //the radius of a beacon consisting of two photons
+                var expectedRadius = Math.sqrt(2 * Math.pow(fresh1.radius, 2));
+                expect(Beam.getRadius(remainingBeam)).to.equal(expectedRadius);
+        });
+});
